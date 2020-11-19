7df21a527c62beefec99ec1e210cba644c7aa3b2
Matvey Sarasov <sarasovmatvej@gmail.com>
2020-11-19 17:06:59 +0600

    added testimonial section without adaptive

--- assets/js/owl-carousel.js
+++ assets/js/owl-carousel.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
     $("#slider").owlCarousel({
         items: 1,
+        autoHeight: true,
+        dots: true,
         nav: true,
         navText: ["<i class='testimonial__nav-icon testimonial__nav-icon_left icon-chevron-left'></i>","<i class='testimonial__nav-icon testimonial__nav-icon_right icon-chevron-right'></i>"]
     });
