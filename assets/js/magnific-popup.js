5817e1a2f31978df84343426034793bfd08f8113
Matvey Sarasov <sarasovmatvej@gmail.com>
2020-11-25 18:17:12 +0600

    added popup in events section

--- assets/js/magnific-popup.js
+++ assets/js/magnific-popup.js
@@ -1,9 +1,10 @@
 $(function () {
 	$('.events__btn').magnificPopup({
 		type: 'inline',
+		showCloseBtn: false,
 		preloader: false,
 		focus: '#username',
-		modal: true
+		closeOnBgClick: true
 	});
 	$(document).on('click', '.events__modal-dismiss', function (e) {
 		e.preventDefault();
